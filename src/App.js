8ca9bd77697820dbad1f3b0d3547a706bb59306f
steve braco <steve.braco@gmail.com>
2021-04-29 14:59:49 +0200

    'finall'

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
 import AboutMeScreen from "./components/Screen/AboutMeScreen/AboutMeScreen";
 import ContactScreen from "./components/Screen/ContactScreen/ContactScreen";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path='/services'  component={ServicesScreen} />
         <Route path='/about'  component={AboutMeScreen} />
         <Route path='/contact'  component={ContactScreen} />
+        <Route component={NotFound}/>
       </Switch>
       <Footer/>
     </Router>
---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFound() {
+    return (
+        <div>
+            <h1 className='not-found'>PAGE NON TROUVÉE</h1>
+        </div>
+    )
+}
